8e1d829070a092242c46fd2e58099dd06095b5e8
Asanka C. Herath <asanka@secure-endpoints.com>
2010-11-11 17:07:02 -0500

    Add string functions

---
+++ string.c
@@ -0,0 +1,53 @@
+#include "heim.h"
+#include <mit-krb5.h>
+#include <roken.h>
+
+mit_krb5_error_code KRB5_CALLCONV
+mit_krb5_timestamp_to_sfstring (mit_krb5_timestamp ts, char *buffer,
+                                size_t buflen, char *pad)
+{
+    size_t      c;
+    struct tm   tm;
+    time_t      timestamp = ts;
+    int         i;
+
+    static const char * const formats[] = {
+	"%c",
+	"%d %b %Y %T",
+	"%x %X",
+	"%d/%m/%Y %R"
+    };
+
+    localtime_r(&timestamp, &tm);
+
+    for (i=0; i < sizeof(formats)/sizeof(formats[0]); i++) {
+        c = strftime(buffer, buflen, formats[i], &tm);
+        if (c > 0 && c < buflen)
+            break;
+    }
+
+    if (c == 0 || c >= buflen)
+        return ENOMEM;
+
+    if (c < buflen - 1 && pad != NULL) {
+        memset(buffer + c, *pad, (buflen - 1) - c);
+        buffer[buflen - 1] = '\0';
+    }
+
+    return 0;
+}
+
+mit_krb5_error_code KRB5_CALLCONV
+mit_krb5_timestamp_to_string (mit_krb5_timestamp ts, char *buffer, size_t buflen)
+{
+    size_t      c;
+    time_t      timestamp = ts;
+    struct tm   tm;
+
+    localtime_r(&timestamp, &tm);
+
+    c = strftime(buffer, buflen, "%c", &tm);
+    if (c == 0 || c >= buflen)
+        return ENOMEM;
+    return 0;
+}
